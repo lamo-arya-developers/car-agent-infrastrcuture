bca9b98784cd178deacc999e8f0ee2faa6dbe330
Ali Kouravand <ali.kouravand@basefarm-orange.com>
2026-04-25 16:00:53 +0200

    forgot to output agentcore ecr url

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "ecr_lambda_url" {
   sensitive = false
   description = "this is leveraged in the CI/CD pipeline to deploy a placeholder image to lambda"
 }
+output "ecr_agentcore_url" {
+  value = module.ecr_agentcore.ecr_agentcore_repo_url
+  sensitive = false
+  description = "this is leveraged in the CI/CD pipeline to deploy a placeholder image to agentcore"
+}
