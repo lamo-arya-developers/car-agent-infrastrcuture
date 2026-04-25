c1a284dd7da4e5ed33987bf862a2dd65e077f90b
Ali Kouravand <ali.kouravand@basefarm-orange.com>
2026-04-25 13:02:46 +0200

    new setup of workflow test

--- config.tf
+++ config.tf
@@ -59,4 +59,3 @@ module "orchestrator_lambda" {
 }
 
 #### NETWORKING RESOURCES ####
-
---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "ecr_lambda_url" {
+  value = module.ecr_lambda.ecr_lambda_repo_url
+  sensitive = true
+  description = "this is leveraged in the CI/CD pipeline to deploy a placeholder image to lambda"
+}
