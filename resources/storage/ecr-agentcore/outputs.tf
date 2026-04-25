6e621af851e4ef2862bb992a304dcb15c20e287e
Ali Kouravand <ali.kouravand@basefarm-orange.com>
2026-04-25 15:31:13 +0200

    testing with false in ecr url output

--- resources/storage/ecr-agentcore/outputs.tf
+++ resources/storage/ecr-agentcore/outputs.tf
@@ -7,5 +7,5 @@ output "ecr_agentcore_repo_arn" {
 output "ecr_agentcore_repo_url" {
   value = aws_ecr_repository.agentcore_ecr.repository_url
   description = "the URL of the agentcore ECR repository"
-  sensitive = true
+  sensitive = false
 }
--- resources/storage/ecr-lambda/outputs.tf
+++ resources/storage/ecr-lambda/outputs.tf
@@ -7,5 +7,5 @@ output "ecr_lambda_repo_arn" {
 output "ecr_lambda_repo_url" {
   value = aws_ecr_repository.orchestrator_lambda.repository_url
   description = "this is the URL of the lambdas ECR Repository where it gets its docker image from"
-  sensitive = true
+  sensitive = false
 }
