0481ac0f32ce73c8e93784657800cec9dcab4a0a
Ali Kouravand <ali.kouravand@basefarm-orange.com>
2026-04-25 12:46:26 +0200

    mutable for images

--- resources/storage/ecr_lambda/main.tf
+++ resources/storage/ecr_lambda/main.tf
@@ -1,5 +1,5 @@
 
 resource "aws_ecr_repository" "orchestrator_lambda" {
   name = var.env == "prod" ? "lambda-ecr-prod" : "lambda-ecr-dev"
-  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+  image_tag_mutability = "MUTABLE"
 }
