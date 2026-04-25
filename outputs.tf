84cb7decc4a499453882100e55e63367c7fb0590
Ali Kouravand <ali.kouravand@basefarm-orange.com>
2026-04-25 13:16:57 +0200

    ne test 2

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "ecr_lambda_url" {
   value = module.ecr_lambda.ecr_lambda_repo_url
-  sensitive = true
+  sensitive = false
   description = "this is leveraged in the CI/CD pipeline to deploy a placeholder image to lambda"
 }
