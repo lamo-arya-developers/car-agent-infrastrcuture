e6be7bb8f07517f0f295c20379500c062d1d46d7
Ali Kouravand <ali.kouravand@basefarm-orange.com>
2026-04-27 13:09:35 +0200

    force delete on all ecr

--- resources/storage/ecr-auth-lambda/main.tf
+++ resources/storage/ecr-auth-lambda/main.tf
@@ -2,4 +2,5 @@
 resource "aws_ecr_repository" "auth_lambda" {
   name = var.env == "prod" ? "auth-lambda-ecr" : "auth-lambda-ecr-dev"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 }
--- resources/storage/ecr-orchestrator-lambda/main.tf
+++ resources/storage/ecr-orchestrator-lambda/main.tf
@@ -2,4 +2,5 @@
 resource "aws_ecr_repository" "orchestrator_lambda" {
   name = var.env == "prod" ? "orchestratorlambda-ecr" : "orchestrator-lambda-ecr-dev"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 }
