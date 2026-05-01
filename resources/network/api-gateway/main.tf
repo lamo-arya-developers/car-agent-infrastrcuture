c0d68c3228dfff3922feb8e6b1ddb080e518ec4f
Ali Kouravand <ali.kouravand@basefarm-orange.com>
2026-05-01 17:48:08 +0200

    fixed errors

--- resources/network/api-gateway/main.tf
+++ resources/network/api-gateway/main.tf
@@ -28,7 +28,7 @@ resource "aws_apigatewayv2_authorizer" "jwt" {
 
   jwt_configuration {
     audience = [var.cognito_user_pool_client_id]
-    issuer   = "https://cognito-idp.${data.aws_region.current.name}.amazonaws.com/${var.cognito_user_pool_id}"
+    issuer   = "https://cognito-idp.${data.aws_region.current.id}.amazonaws.com/${var.cognito_user_pool_id}"
   }
 }
 
